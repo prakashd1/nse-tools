e4dc355bfec33e482a1e2666981666037cd44b4b
Prakash Dayaramani <prakash.dayaramani@gmail.com>
2019-01-01 21:46:45 +0530

    Added nn

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 export default function App() {
   console.log("something is happening");
+  const net = new brain.NeuralNetwork({
+    hiddenLayers: [3]
+  });
+  const trainingData = [
+    { input: [0, 0], output: [0] },
+    { input: [0, 1], output: [1] },
+    { input: [1, 0], output: [1] },
+    { input: [1, 1], output: [0] }
+  ];
+
+  net.train(trainingData);
+  var v = net.run([0, 0]);
+  console.log(v);
   return (
     <div className="App">
-      <h1>Work in progress</h1>
+      <h1>{v}</h1>
     </div>
   );
 }
